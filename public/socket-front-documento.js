eb7dea102556973c5342fffd14d64adcda4bdac1
José Eduardo <157645066+joeduardo10@users.noreply.github.com>
2025-01-11 12:53:45 -0300

    "3"

--- public/socket-front-documento.js
+++ public/socket-front-documento.js
@@ -3,16 +3,22 @@ import { atualizarTextoEditor } from "./documento.js";
 const socket = io();
 
 function selecionardocumento(nome){
-    socket.emit("selecionar_documento", nome);
+    socket.emit("selecionar_documento", nome,(texto) =>{
+        atualizarTextoEditor(texto);
+    });
 }
 
 function emitirTextoEditor(dados){ 
     socket.emit("Texto_Editor", dados);
 }
 
-socket.on("Texto_Editor_Clientes", (texto) => {
+socket.on("texto_documento", (texto) => {
     atualizarTextoEditor(texto);
 });
+
+// socket.on("Texto_Editor_Clientes", (texto) => {
+//     atualizarTextoEditor(texto);
+// });
 
 socket.on("disconnect", (motivo) => {
     console.log(`Servidor desconectado!
--- src/socket-back.js
+++ src/socket-back.js
@@ -1,15 +1,49 @@
 import io from "./servidor.js";
 
+const documentos = [
+    {
+        nome: "JavaScript",
+        texto: "texto de javascript...",
+    },
+    {
+        nome: "Node",
+        texto: " texto de Node...",
+    },
+    {
+        nome: "Socket.io",
+        texto: "texto de socket.io...",
+    },
+];
 io.on("connection", (socket) => {
     console.log("usuário conectado ID", socket.id);
-    socket.on("selecionar_documento", (nomeDocumento) => {
+    socket.on("selecionar_documento", (nomeDocumento, devolverTexto) => {
         socket.join(nomeDocumento);
+
+        const documento = encontrarDocumento(nomeDocumento);
+
+        if (documento) {
+            // socket.emit("texto_documento", documento.texto);
+            devolverTexto(documento.texto);
+        }
+        
     })
     // socket.on("disconnect", (motivo) => {
     //     console.log(`Cliente "${socket.id}" desconectado!
     //     Motivo: ${motivo}`);
     // });
     socket.on("Texto_Editor", ({texto,nomeDocumento}) => {
-        socket.to(nomeDocumento).emit("Texto_Editor_Clientes", texto);
+        const documento = encontrarDocumento(nomeDocumento);
+        if(documento){
+            documento.texto = texto;
+            socket.to(nomeDocumento).emit("Texto_Editor_Clientes", texto);
+        }
     });
 });
+
+function encontrarDocumento(nome){
+    const documento = documentos.find((documento) =>{
+        return documento.nome === nome;
+    });
+    return documento;
+
+}
